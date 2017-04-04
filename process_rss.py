f81e936758e08a5a53a0c9e5f227e4e48087775c
Mike Melanson <mike@multimedia.cx>
2017-04-03 20:47:20 -0700

    Initial commit

---
+++ process_rss.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+
+import commands
+import dbm
+import json
+import requests
+import StringIO
+import sys
+import xml.etree.ElementTree as ET
+
+YOUTUBE_DL = "/usr/local/bin/youtube-dl"
+
+def derive_download_links(input_xml):
+    cache = dbm.open("yt2pod.cache", "c")
+
+    xml_string_file = StringIO.StringIO(input_xml)
+    tree = ET.parse(xml_string_file)
+    root = tree.getroot()
+
+    for child in root:
+        if child.tag.endswith('entry'):
+            # pick out the main YouTube URL
+            link_elem = child.find('{http://www.w3.org/2005/Atom}link')
+            yt_link = link_elem.get("href")
+            if yt_link in cache:
+                cached = json.loads(cache[yt_link])
+                dl_link = cached['dl_link']
+                dl_size = cached['dl_size']
+            else:
+                # invoke youtube-dl to fetch the download link
+                cmd = YOUTUBE_DL + " --get-url --format 140 " + yt_link
+                (status, dl_link) = commands.getstatusoutput(cmd)
+
+                # invoke requests module with a HEAD request to get size
+                head_req = requests.head(dl_link)
+                dl_size = head_req.headers['Content-Length']
+
+                # cache the downloaded information
+                cached = { 'dl_link': dl_link,
+                    'dl_size': dl_size
+                }
+                cache[yt_link] = json.dumps(cached)
+
+            # create a new element and insert into the item
+            attrib = { 'url': dl_link,
+                'type': 'audio/mpeg',
+                'length': dl_size
+            }
+            enclosure = ET.Element("enclosure", attrib)
+            child.insert(0, enclosure)
+
+            # remove irrelevant elements
+            child.remove(link_elem)
+            elem = child.find('{http://www.w3.org/2005/Atom}author')
+            child.remove(elem)
+            elem = child.find('{http://www.w3.org/2005/Atom}id')
+            child.remove(elem)
+
+    output_xml = StringIO.StringIO()
+    tree.write(output_xml, encoding="UTF-8", xml_declaration=True)
+    return output_xml.getvalue()
+
+def transform_rss_xml(input_xml, verbose=False, refresh=False):
+    cache = dbm.open("yt2pod.cache", "c")
+
+    xml_string_file = StringIO.StringIO(input_xml)
+    in_tree = ET.parse(xml_string_file)
+    in_root = in_tree.getroot()
+
+    # establish output RSS framework
+    root_rss = ET.Element('rss', attrib={'version': '2.0'})
+    out_tree = ET.ElementTree(root_rss)
+    channel = ET.Element('channel')
+    root_rss.append(channel)
+
+    # transfer the top-level metadata
+    in_title = in_root.find('{http://www.w3.org/2005/Atom}title')
+    out_title = ET.Element('title')
+    out_title.text = in_title.text
+    channel.append(out_title)
+    description = ET.Element('description')
+    description.text = "YouTube audio-only feed for '%s'" % (out_title.text)
+    channel.append(description)
+    published = in_root.find('{http://www.w3.org/2005/Atom}published')
+    lastBuildDate = ET.Element('published')
+    lastBuildDate.text = published.text
+    channel.append(lastBuildDate)
+
+    # iterate through the items and transfer each
+    for child in in_root:
+        if child.tag.endswith('entry'):
+            item = ET.Element('item')
+            media_group = child.find('{http://search.yahoo.com/mrss/}group')
+            # transfer metadata - title
+            in_title = child.find('{http://www.w3.org/2005/Atom}title')
+            out_title = ET.Element('title')
+            out_title.text = in_title.text
+            item.append(out_title)
+            # description
+            in_desc = media_group.find('{http://search.yahoo.com/mrss/}description')
+            out_desc = ET.Element('description')
+            out_desc.text = in_desc.text
+            item.append(out_desc)
+            # published -> pubDate
+            in_date = child.find('{http://www.w3.org/2005/Atom}published')
+            out_date = ET.Element('pubDate')
+            out_date.text = in_date.text
+            item.append(out_date)
+            # thumbnail
+#            in_thumb = media_group.find('{http://search.yahoo.com/mrss/}thumbnail')
+#            out_thumb = ET.Element('thumbnail')
+#            out_thumb.attrib = in_thumb.attrib
+#            item.append(out_thumb)
+
+            # pick out the main YouTube URL
+            link_elem = child.find('{http://www.w3.org/2005/Atom}link')
+            yt_link = link_elem.get("href")
+            if verbose:
+                print "Fetching " + yt_link + "...",
+            if not refresh and yt_link in cache:
+                if verbose:
+                    print "cached"
+                cached = json.loads(cache[yt_link])
+                dl_link = cached['dl_link']
+                dl_size = cached['dl_size']
+            else:
+                # invoke youtube-dl to fetch the download link
+                cmd = YOUTUBE_DL + " --get-url --format 140 " + yt_link
+                if verbose:
+                    print "caching..."
+                (status, dl_link) = commands.getstatusoutput(cmd)
+
+                # invoke requests module with a HEAD request to get size
+                head_req = requests.head(dl_link)
+                dl_size = head_req.headers['Content-Length']
+
+                # cache the downloaded information
+                cached = { 'dl_link': dl_link,
+                    'dl_size': dl_size
+                }
+                cache[yt_link] = json.dumps(cached)
+
+            # create a new element and insert into the item
+            attrib = { 'url': dl_link,
+                'type': 'audio/mpeg',
+                'length': dl_size
+            }
+            enclosure = ET.Element("enclosure", attrib)
+            item.append(enclosure)
+            channel.append(item)
+
+    output_xml = StringIO.StringIO()
+    out_tree.write(output_xml, encoding="utf8", xml_declaration=True)
+    return output_xml.getvalue()
+
---
+++ yt2pod-server.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+
+import BaseHTTPServer
+import commands
+import getopt
+import json
+import requests
+import sys
+
+import process_rss
+
+DEFAULT_HTTP_PORT = 54321
+FEED_BASE_URL = "https://www.youtube.com/feeds/videos.xml?"
+CONF_JSON = "yt2pod.conf.json"
+
+feed_list = {}
+
+def get_yt_rss_feed(req_path):
+    if req_path.startswith('/videos/'):
+        feed_url = FEED_BASE_URL + "channel_id=" + req_path[8:]
+    else:
+        feed_url = FEED_BASE_URL + "playlist_id=" + req_path[10:]
+    r = requests.get(feed_url)
+    return (r.status_code, r.text)
+
+class YT2PodHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/":
+            # list index
+            html = "<html><head><title>Feed List</title></head>\n"
+            html += "<body><ul>"
+            for feed in feed_list:
+                html += "<li><a href='/" + feed['type'] + "/" + feed['uid'] + "'>" + feed['name'] + "</a></li>\n"
+            html += "</body></html>"
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(html)
+            
+        elif self.path.startswith('/videos/') or self.path.startswith('/playlist/'):
+            (status_code, rss_xml) = get_yt_rss_feed(self.path)
+            self.send_response(status_code)
+            self.send_header("Content-type", "text/xml")
+            self.end_headers()
+            if (status_code == 200):
+                self.wfile.write(process_rss.transform_rss_xml(rss_xml.encode('utf8')))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Path '%s' does not exist" % (self.path))
+
+def run(port,
+        server_class=BaseHTTPServer.HTTPServer,
+        handler_class=YT2PodHandler):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print "YT2Pod server running on port %d... (Ctrl-C to exit)" % (port)
+    httpd.serve_forever()
+
+
+def usage():
+    print """USAGE: yt2pod-server.py <options>
+  -h, --help: Print this message
+  -p, --port=[number]: Port on which to run the HTTP server
+  -c, --cache: Request to pre-process a feed into the cache
+"""
+
+if __name__ == '__main__':
+
+    port = DEFAULT_HTTP_PORT
+    feed_list = json.loads(open(CONF_JSON, "r").read())
+
+    # process command line arguments
+    opts, args = getopt.getopt(sys.argv[1:], "chp:r", ["cache", "help", "port=", "refresh"])
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-p", "--port"):
+            try:
+                port = int(arg)
+            except ValueError:
+                print "Invalid port number"
+                sys.exit()
+        elif opt in ("-c", "--cache", "-r", "--refresh"):
+            force_refresh = False
+            if opt in ("-r", "--refresh"):
+                force_refresh = True
+            print "Select a feed to preprocess into the cache:"
+            n = 1
+            for feed in feed_list:
+                print "%d: %s (%s)" % (n, feed['name'], feed['type'])
+                n += 1
+            choice = int(input()) - 1
+            if choice < 0 or choice >= len(feed_list):
+                print "choice out of range %d" % (choice + 1)
+                sys.exit(1)
+            feed = feed_list[choice]
+            feed_url = "/" + feed['type'] + "/" + feed['uid']
+            print "caching '%s': '%s'" % (feed['name'], feed_url)
+            (status_code, rss_xml) = get_yt_rss_feed(feed_url)
+            process_rss.transform_rss_xml(rss_xml.encode('utf8'), verbose=True, refresh=force_refresh)
+            sys.exit(0)
+
+    # kick off the server
+    run(port)
